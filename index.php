0ebbe46401403fbf3688b24d54b15b5a8710ac0a
Aribunfi <arianna@pabis.de>
2023-03-15 06:44:20 +0100

    numeri progressivi hotel

--- index.php
+++ index.php
@@ -49,40 +49,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Hotel</title>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </head>
 <body>
 <div class="container">
     <div class="row">
         <div class="col-12">
+
             <div class="card">
   <div class="card-header">
                 <h1>PHP HOTEL, verifica disponibilità:</h1>
 
   </div>
   <div class="card-body">
+
          <table class="table table-striped">
             <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Distance to center</th>
     </tr>
   </thead>
+
   <tbody>
+  <?php foreach($hotels as $key => $hotel) : ?>
     <tr>
-      <th scope="row">1</th>
+      <th scope="row"><?= $key + 1 ?></th>
       <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
     </tr>
-    <tr>
+    <?php endforeach; ?>
+  </tbody>
             </table>
         </div>
   </div>
-</div>
+
+
+
+
+
        
     </div>
 </div>
